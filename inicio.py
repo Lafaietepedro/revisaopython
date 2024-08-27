95429a382e91ff6fcc806efec840a0bdeaeb623f
Lafaietepedro <lafaietepedro3@gmail.com>
2024-08-27 16:59:23 -0300

    update

--- inicio.py
+++ inicio.py
@@ -2,5 +2,5 @@ from ex3 import Aluno
 
 a = Aluno(200,10)
 
-print(f'Percentual de faltas: {a.calcula_faltas():.1f}\n')
+print(f'Faltas permitidas: {a.calcula_faltas():.0f}\n')
 print(f'{a.verifica_aprovacao()}')
